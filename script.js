d8e07fe72b61be841a221b6dac2613e9ebe71e40
georginklv <georginklv@abv.bg>
2019-09-29 23:06:02 +0300

    fix: final

--- script.js
+++ script.js
@@ -27,14 +27,81 @@ function start() {
     gameArea.appendChild(bird);
     player.x = bird.offsetLeft;
     player.y = bird.offsetTop;
+    player.pipe = 0;
+    let spacing = 300;
+    let howMany = Math.floor((gameArea.offsetWidth) / spacing);
+    console.log(howMany);
+    for (let x = 0; x < howMany; x++) {
+        buildPipes(player.pipe * spacing);
+    }
     window.requestAnimationFrame(playGame);
 }
 
+function buildPipes(startPos) {
+    let totalHeight = gameArea.offsetHeight;
+    let totalWidth = gameArea.offsetWidth;
+    player.pipe++;
+    let pipe1 = document.createElement("div");
+    pipe1.start = startPos + totalWidth;
+    pipe1.classList.add("pipe");
+    pipe1.innerHTML = "<br>" + player.pipe;
+    pipe1.height = Math.floor(Math.random() * 350);
+    pipe1.style.height = pipe1.height + "px";
+    pipe1.style.left = pipe1.start + "px";
+    pipe1.style.top = "0px";
+    pipe1.x = pipe1.start;
+    pipe1.id = player.pipe;
+    pipe1.style.backgroundColor = "red";
+    gameArea.appendChild(pipe1);
+    let pipeSpace = Math.floor(Math.random() * 250) + 150;
+    let pipe2 = document.createElement("div");
+    pipe2.start = pipe1.start;
+    pipe2.classList.add("pipe");
+    pipe2.innerHTML = "<br>" + player.pipe;
+    pipe2.style.height = totalHeight - pipe1.height - pipeSpace + "px";
+    pipe2.style.left = pipe1.start + "px";
+    pipe2.style.bottom = "0px";
+    pipe2.x = pipe1.start;
+    pipe2.id = player.pipe;
+    pipe2.style.backgroundColor = "red";
+    gameArea.appendChild(pipe2);
+}
+
+function movePipes(bird) {
+    let lines = document.querySelectorAll(".pipe");
+    let counter = 0; //counts pips to remove
+    lines.forEach(function (item) {
+        console.log(item);
+        item.x -= player.speed;
+        item.style.left = item.x + "px";
+        if (item.x < 0) {
+            item.parentElement.removeChild(item);
+            counter++; 
+        }
+        if(isCollide(item,bird)){
+            playGameOver(bird);
+        }
+    })
+    counter = counter / 2;
+    for (let x = 0; x < counter; x++) {
+        buildPipes(0);
+    }
+}
+
+function isCollide(a,b){
+    let aRect = a.getBoundingClientRect();
+    let bRect = b.getBoundingClientRect();
+    console.log(aRect);
+    return!(
+        (aRect.bottom <bRect.top)||(aRect.top >bRect.bottom)||(aRect.right < bRect.left)||(aRect.left >bRect.right)
+    )
+}
+
 function playGame() {
-    console.log(player);
     if (player.inplay) {
         let bird = document.querySelector(".bird");
         let wing = document.querySelector(".wing");
+        movePipes(bird);
         let move = false;
         if (keys.ArrowLeft && player.x > 0) {
             player.x -= player.speed;
